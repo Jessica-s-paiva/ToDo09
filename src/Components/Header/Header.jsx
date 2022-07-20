936f8af6b666faca07f7489e352f5f252a96cd90
Jessica-s-paiva <jessicapaivamvp@gmail.com>
2022-07-20 17:10:16 -0300

    feat: criando componente Header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import S from './Header.module.css';
+const Header = () => {
+  return (
+    <header>
+        <h4>Uma seleção de produtos</h4>
+        <h1>Especial para você</h1>
+        <p>Todos os produtos desta lista foram selecionados a partir da sua navegação. Aproveite!</p>
+    </header>
+  )
+}
+
+export default Header
